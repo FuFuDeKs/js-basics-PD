f2d668e29c59f4342aa62161d21586ee93262a10
Edgars G <Edgars@gmail>
2025-10-27 11:49:00 +0200

    added store uzdevums OTRA REIZE

--- internet-store.js
+++ internet-store.js
@@ -3,12 +3,12 @@ let sum = 0;
 
 let products = 
 [
-    {name: "T-shirt", category: "clothing", daudzums: 20 },
-    {name: "Laptop", category: "electronics", daudzums: 17 },
-    {name: "jeans", category: "clothing", daudzums: 56 },
-    {name: "PS5", category: "electronics", daudzums: 3 },
-    {name: "caps", category: "clothing", daudzums: 201 },
-    {name: "Xbox", category: "electronics", daudzums: 2 }
+    {name: "T-shirt", category: "clothing", daudzums: 20, cena: 13 },
+    {name: "Laptop", category: "electronics", daudzums: 17, cena: 799 },
+    {name: "jeans", category: "clothing", daudzums: 56, cena: 25 },
+    {name: "PS5", category: "electronics", daudzums: 3, cena: 699 },
+    {name: "caps", category: "clothing", daudzums: 201, cena: 10 },
+    {name: "Xbox", category: "electronics", daudzums: 2, cena: 500}
 ]
 
 for(let i = 0; i < products.length; i++)
@@ -18,7 +18,7 @@ for(let i = 0; i < products.length; i++)
                 console.log("Elektronika ir: " + produkti.name);
             }
 
-sum += produkti.daudzums;
-    console.log("Kopejais preču daudzums noliktāvā:" + sum)
+sum += produkti.cena;
+    console.log("KOpeja summa: " + sum);
 
     }
